863e00867ac5a9af3caedc7fa6039554e94ac42e
Sohil Khan <zenon.byte@gmail.com>
2024-01-24 22:44:32 +0530

    feat: add initial app template

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+
+export default function Header() {
+    return (
+        <header id="main-header">
+            <div id="title">
+                <img src={logoImg} alt="A restaurant" />
+                <h1>ReactFood</h1>
+            </div>
+            <nav>
+                <Button textOnly>Card (0)</Button>
+            </nav>
+        </header>
+    );
+}
